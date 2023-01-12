703a12d8a17cd391654e2d6a034106159b4129ad
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2023-01-12 16:27:02 +0100

    feat(drinks): add more drinks

--- imbibe/Data/Drinks.swift
+++ imbibe/Data/Drinks.swift
@@ -51,6 +51,50 @@ class Drinks {
             source: nil
         )
     
+    static let darkAndStormy: Drink =
+        .init(
+            name: "Dark and Stormy",
+            description: "",
+            origin: Origin(name: "Bermuda", flag: "🇧🇲"),
+            equipments: [ Equipments.highballGlass ],
+            ingredients: [
+                .init(ingredient: Ingredients.darkRum, volumeInML: 60),
+                .init(ingredient: Ingredients.gingerBeer, volumeInML: 90),
+                .init(ingredient: Ingredients.ice),
+                .init(ingredient: Ingredients.lime),
+            ],
+            steps: [
+                .init(string: "Fill up the Highball Glass with Ice Cubes"),
+                .init(string: "Add Dark Rum & Ginger Beer"),
+                .init(string: "Garnish with a Lime Wedge"),
+            ],
+            source: nil
+        )
+    
+    static let ginFizz: Drink =
+        .init(
+            name: "Gin Fizz",
+            description: "",
+            origin: Origin(name: "United Kingdom", flag: "🇬🇧"),
+            equipments: [ Equipments.highballGlass, Equipments.shaker ],
+            ingredients: [
+                .init(ingredient: Ingredients.gin, volumeInML: 60),
+                .init(ingredient: Ingredients.lemonJuice, volumeInML: 30),
+                .init(ingredient: Ingredients.sodaWater, volumeInML: 80),
+                .init(ingredient: Ingredients.simpleSirup, volumeInML: 10),
+                .init(ingredient: Ingredients.iceCubes),
+            ],
+            steps: [
+                .init(string: "Fill the Shaker with Ice Cubes"),
+                .init(string: "Add Gin, Lemon Juice, & Simple Sirup"),
+                .init(string: "Shake well (10-15 seconds)"),
+                .init(string: "Strain into the Cocktail Glass"),
+                .init(string: "Top up with Soda Water"),
+                .init(string: "Garnish with a Lemon Slice"),
+            ],
+            source: nil
+        )
+    
     static let manhattan: Drink =
         .init(
             name: "Manhattan",
@@ -69,6 +113,27 @@ class Drinks {
                 .init(string: "Stir"),
                 .init(string: "Strain into the Cocktail Class"),
                 .init(string: "Garnish with Cocktail Cherry"),
+            ],
+            source: nil
+        )
+    
+    static let martini: Drink =
+        .init(
+            name: "Martini",
+            description: "",
+            origin: Origin(name: "United States", flag: "🇺🇸"),
+            equipments: [ Equipments.cocktailGlass, Equipments.barspoon, Equipments.mixingGlass ],
+            ingredients: [
+                .init(ingredient: Ingredients.gin, volumeInML: 60),
+                .init(ingredient: Ingredients.dryVermouth, volumeInML: 10),
+                .init(ingredient: Ingredients.iceCubes),
+            ],
+            steps: [
+                .init(string: "Add Ice Cubes to the Mixing Glass"),
+                .init(string: "Add Gin & Dry Vermouth"),
+                .init(string: "Stir together with Barspoon"),
+                .init(string: "Strain into the Cocktail Glass"),
+                .init(string: "Garnish with Olives and a Lemon Twist"),
             ],
             source: nil
         )
@@ -95,6 +160,26 @@ class Drinks {
                 .init(string: "Fill the Highball Glass with Ice Cubes, and strain contents of the shaker into the glass"),
                 .init(string: "With Barspoon stir to shuffle the Mint Leaves around"),
                 .init(string: "Top op with Soda Water"),
+            ],
+            source: nil
+        )
+    
+    static let moscowMule: Drink =
+        .init(
+            name: "Moscow Mule",
+            description: "",
+            origin: Origin(name: "United States", flag: "🇺🇸"),
+            equipments: [ Equipments.highballGlass, Equipments.barspoon ],
+            ingredients: [
+                .init(ingredient: Ingredients.vodka, volumeInML: 60),
+                .init(ingredient: Ingredients.limeJuice, volumeInML: 10),
+                .init(ingredient: Ingredients.gingerBeer, volumeInML: 120),
+                .init(ingredient: Ingredients.iceCubes),
+            ],
+            steps: [
+                .init(string: "Fill Highball Glass with Ice Cubes"),
+                .init(string: "Add Vodka, Ginger Beer, & Lime Juice into the Highball Glass"),
+                .init(string: "Garnish with Lime Wedge"),
             ],
             source: nil
         )
@@ -141,6 +226,50 @@ class Drinks {
             source: nil
         )
     
+    static let sazerac: Drink =
+        .init(
+            name: "Sazerac",
+            description: "",
+            origin: Origin(name: "United States", flag: "🇺🇸"),
+            equipments: [ Equipments.mixingGlass, Equipments.barspoon, Equipments.oldFashionedGlass ],
+            ingredients: [
+                .init(ingredient: Ingredients.cognac, volumeInML: 60),
+                .init(ingredient: Ingredients.absinthe, volumeInML: 10),
+                .init(ingredient: Ingredients.simpleSirup, volumeInML: 15),
+                .init(ingredient: Ingredients.peychaudBitters, dashes: 2),
+                .init(ingredient: Ingredients.iceCubes),
+            ],
+            steps: [
+                .init(string: "Fill the Old Fashioned Glass with Ice Cubes"),
+                .init(string: "In the Mixing Glass add Cognac, Absinthe, Simple Sirup, & Peychaud Bitters"),
+                .init(string: "Stir with Barspoon"),
+                .init(string: "Strain into the Old Fashioned Glass"),
+                .init(string: "Garnish with a Lemon Twist"),
+            ],
+            source: nil
+        )
+    
+    static let sidecar: Drink =
+        .init(
+            name: "Sidecar",
+            description: "",
+            origin: Origin(name: "France", flag: "🇫🇷"),
+            equipments: [ Equipments.shaker, Equipments.cocktailGlass ],
+            ingredients: [
+                .init(ingredient: Ingredients.cognac, volumeInML: 60),
+                .init(ingredient: Ingredients.curacao, volumeInML: 20),
+                .init(ingredient: Ingredients.lemonJuice, volumeInML: 20),
+                .init(ingredient: Ingredients.iceCubes),
+            ],
+            steps: [
+                .init(string: "Fill Shaker with Ice Cubes"),
+                .init(string: "Add Cognac, Curacao,  & Lemon Juice"),
+                .init(string: "Shake for 10-15 seconds"),
+                .init(string: "Strain into the Cocktail Glass"),
+            ],
+            source: nil
+        )
+    
     static let whiskySour: Drink =
         .init(
             name: "Whisky Sour",
@@ -169,19 +298,25 @@ class Drinks {
     static let all: [Drink] = [
         americano,
         daiquiri,
+        darkAndStormy,
+        ginFizz,
         manhattan,
+        martini,
+        mojito,
+        moscowMule,
         negroni,
         oldFashioned,
+        sazerac,
+        sidecar,
         whiskySour,
-        mojito,
     ]
     
     static let allIconic: [Drink] = [
         daiquiri,
         negroni,
         oldFashioned,
-        whiskySour,
         mojito,
+        whiskySour,
     ]
     
     static func random() -> Drink {
