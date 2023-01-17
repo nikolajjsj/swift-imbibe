c9c3467d3cff6289d5557fdd6049ee32b3ffd4dd
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2023-01-17 11:56:38 +0100

    refactor(drinkFilters): improve drinkFilters & UserDefaultsHelper class

--- imbibe/Environments/UserDefaults.swift
+++ imbibe/Environments/UserDefaults.swift
@@ -7,6 +7,23 @@
 
 import Foundation
 
-enum UserDefaultKeys: String {
-    case defaultUnit = "default_unit"
+enum UserDefaultKeys: String, CaseIterable {
+    case unit = "default_unit"
+    case drinkStrength = "default_drink_strength"
+    case drinkEra = "default_drink_era"
+}
+
+final class UserDefaultsHelper {
+    static func set<T>(value: T, key: UserDefaultKeys) {
+        UserDefaults.standard.set(value, forKey: key.rawValue)
+    }
+    
+    static func get<T>(type: T.Type, forKey: UserDefaultKeys) -> T? {
+        let value = UserDefaults.standard.object(forKey: forKey.rawValue) as? T
+        return value
+    }
+    
+    static func remove(key: UserDefaultKeys) {
+        UserDefaults.standard.removeObject(forKey: key.rawValue)
+    }
 }
--- imbibe/Features/Drinks/DrinkFilters.swift
+++ imbibe/Features/Drinks/DrinkFilters.swift
@@ -7,73 +7,10 @@
 
 import SwiftUI
 
-private enum Strength: String, CaseIterable {
-    case light = "Light"
-    case medium = "Medium"
-    case strong = "Strong"
-    
-    func compareDrink(_ d: Drink) -> Bool {
-        switch self {
-        case .light:
-            return d.strength < 10
-        case .medium:
-            return d.strength >= 10 && d.strength < 20
-        case .strong:
-            return d.strength >= 20
-        }
-    }
-}
-
-private enum Era: String, CaseIterable {
-    case archaic = "Archaic"
-    case golden = "Golden"
-    case prohibition = "Prohibition"
-    case postProhibition = "Post Prohibition"
-    case cocktailReivival = "Cocktail Revival"
-    case modernCocktail = "Modern Cocktail"
-    
-    func compareDrink(_ d: Drink) -> Bool {
-        guard let year = d.year else { return false }
-        switch self {
-        case .archaic:
-            return year < 1860
-        case .golden:
-            return year >= 1860 && year < 1920
-        case .prohibition:
-            return year >= 1920 && year < 1933
-        case .postProhibition:
-            return year >= 1933 && year < 1980
-        case .cocktailReivival:
-            return year >= 1980 && year < 2005
-        case .modernCocktail:
-            return year >= 2005
-        }
-    }
-    
-    var label: String {
-        switch self {
-        case .archaic:
-            return self.rawValue + " (until 1860)"
-        case .golden:
-            return self.rawValue + " (1860 - 1920)"
-        case .prohibition:
-            return self.rawValue + " (1920 - 1933)"
-        case .postProhibition:
-            return self.rawValue + " (1933 - 1980)"
-        case .cocktailReivival:
-            return self.rawValue + " (1980 - 2005)"
-        case .modernCocktail:
-            return self.rawValue + " (from 2005)"
-        }
-    }
-}
 
 struct DrinkFilters: View {
     @Environment(\.dismiss) var dismiss
-    
-    @State private var drinks: [Drink] = Drinks.all
-    @State private var strengths: [Strength] = []
-    @State private var eras: [Era] = []
+    @ObservedObject private var vm = ViewModel()
     
     let onChanged: ([Drink]) -> ()
     
@@ -86,11 +23,7 @@ struct DrinkFilters: View {
                 Spacer()
                 Text("Filters").font(.headline)
                 Spacer()
-                Button(role: .destructive, action: {
-                    strengths.removeAll()
-                    eras.removeAll()
-                    filteredByFilters()
-                }, label: {
+                Button(role: .destructive, action: { vm.clearAll() }, label: {
                     Text("Clear")
                 })
             }
@@ -101,42 +34,34 @@ struct DrinkFilters: View {
                 VStack(alignment: .leading) {
                     Text("Strength").font(.headline)
                     LazyVStack {
-                        ForEach(Strength.allCases, id: \.self) { strength in
-                            let selected = strengths.contains(strength)
-                            FilterOption(label: strength.rawValue.description, selected: selected) {
-                                if selected {
-                                    strengths = strengths.filter({ $0 != strength })
-                                } else {
-                                    strengths.append(strength)
-                                }
-                                filteredByFilters()
-                            }
+                        ForEach(DrinkFilters.ViewModel.Strength.allCases, id: \.self) { strength in
+                            FilterOption(
+                                label: strength.rawValue.description,
+                                selected: vm.strengths.contains(strength),
+                                onChanged: { vm.updateStrengths(strength) }
+                            )
                         }
                     }.padding(.bottom, 32)
                     
                     Text("Era").font(.headline)
                     LazyVStack {
-                        ForEach(Era.allCases, id: \.self) { era in
-                            let selected = eras.contains(era)
-                            FilterOption(label: era.label, selected: selected) {
-                                if selected {
-                                    eras = eras.filter({ $0 != era })
-                                } else {
-                                    eras.append(era)
-                                }
-                                filteredByFilters()
-                            }
+                        ForEach(DrinkFilters.ViewModel.Era.allCases, id: \.self) { era in
+                            FilterOption(
+                                label: era.label,
+                                selected: vm.eras.contains(era),
+                                onChanged: { vm.updateEras(era) }
+                            )
                         }
                     }
                 }.padding(.horizontal)
             }
             
             Button {
-                onChanged(drinks)
+                onChanged(vm.drinks)
                 dismiss()
             } label: {
                 Spacer()
-                Text("Done (\(drinks.count) Drinks)")
+                Text("Done (\(vm.drinks.count) Drinks)")
                     .font(.title2.bold())
                     .padding(.vertical, 8)
                 Spacer()
@@ -161,17 +86,124 @@ struct DrinkFilters: View {
         .buttonStyle(.plain)
         .padding(.vertical, 6)
     }
-    
-    func filteredByFilters() {
-        if strengths.isEmpty && eras.isEmpty {
-            drinks = Drinks.all
-            return
+}
+
+extension DrinkFilters {
+    @MainActor
+    final class ViewModel: ObservableObject {
+        @Published private(set) var drinks: [Drink] = Drinks.all
+        @Published private(set) var strengths: [Strength] = []
+        @Published private(set) var eras: [Era] = []
+        
+        init() {
+            if let strengths = UserDefaultsHelper.get(type: [String].self, forKey: .drinkStrength) {
+                self.strengths = strengths.map{ Strength(rawValue: $0) }.compactMap{ $0 }
+            }
+            if let eras = UserDefaultsHelper.get(type: [String].self, forKey: .drinkEra) {
+                self.eras = eras.map{ Era(rawValue: $0) }.compactMap{ $0 }
+            }
+            
+            filteredByFilters()
         }
         
-        drinks = Drinks.all.filter({ drink in
-            strengths.first(where: { $0.compareDrink(drink) }) != nil ||
-            eras.first(where: { $0.compareDrink(drink) }) != nil
-        })
+        func updateStrengths(_ strength: Strength) {
+            if strengths.contains(strength) {
+                strengths = strengths.filter({ $0 != strength })
+            } else {
+                strengths.append(strength)
+            }
+            UserDefaultsHelper.set(value: strengths.map({ $0.rawValue }), key: .drinkStrength)
+            filteredByFilters()
+        }
+        
+        func updateEras(_ era: Era) {
+            if eras.contains(era) {
+                eras = eras.filter({ $0 != era })
+            } else {
+                eras.append(era)
+            }
+            UserDefaultsHelper.set(value: eras.map({ $0.rawValue }), key: .drinkEra)
+            filteredByFilters()
+        }
+        
+        func clearAll() {
+            strengths.removeAll()
+            eras.removeAll()
+            filteredByFilters()
+        }
+        
+        private func filteredByFilters() {
+            if strengths.isEmpty && eras.isEmpty {
+                drinks = Drinks.all
+                return
+            }
+            
+            drinks = Drinks.all.filter({ drink in
+                strengths.first(where: { $0.compareDrink(drink) }) != nil ||
+                eras.first(where: { $0.compareDrink(drink) }) != nil
+            })
+        }
+        
+        enum Strength: String, CaseIterable {
+            case light = "Light"
+            case medium = "Medium"
+            case strong = "Strong"
+            
+            func compareDrink(_ d: Drink) -> Bool {
+                switch self {
+                case .light:
+                    return d.strength < 10
+                case .medium:
+                    return d.strength >= 10 && d.strength < 20
+                case .strong:
+                    return d.strength >= 20
+                }
+            }
+        }
+        
+        enum Era: String, CaseIterable {
+            case archaic = "Archaic"
+            case golden = "Golden"
+            case prohibition = "Prohibition"
+            case postProhibition = "Post Prohibition"
+            case cocktailReivival = "Cocktail Revival"
+            case modernCocktail = "Modern Cocktail"
+            
+            func compareDrink(_ d: Drink) -> Bool {
+                guard let year = d.year else { return false }
+                switch self {
+                case .archaic:
+                    return year < 1860
+                case .golden:
+                    return year >= 1860 && year < 1920
+                case .prohibition:
+                    return year >= 1920 && year < 1933
+                case .postProhibition:
+                    return year >= 1933 && year < 1980
+                case .cocktailReivival:
+                    return year >= 1980 && year < 2005
+                case .modernCocktail:
+                    return year >= 2005
+                }
+            }
+            
+            var label: String {
+                switch self {
+                case .archaic:
+                    return self.rawValue + " (until 1860)"
+                case .golden:
+                    return self.rawValue + " (1860 - 1920)"
+                case .prohibition:
+                    return self.rawValue + " (1920 - 1933)"
+                case .postProhibition:
+                    return self.rawValue + " (1933 - 1980)"
+                case .cocktailReivival:
+                    return self.rawValue + " (1980 - 2005)"
+                case .modernCocktail:
+                    return self.rawValue + " (from 2005)"
+                }
+            }
+        }
     }
 }
 
