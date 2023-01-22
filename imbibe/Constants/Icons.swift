c21e3ae77f037c90ba32a470de1c3803fafea2ac
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2023-01-22 11:35:45 +0100

    feat(icons): add Icons class

---
+++ imbibe/Constants/Icons.swift
@@ -0,0 +1,20 @@
+//
+//  Icons.swift
+//  imbibe
+//
+//  Created by Nikolaj Johannes Skole Jensen on 21/01/2023.
+//
+
+import Foundation
+
+final class Icons {
+    // Bottom bar icons
+    static let discover: String = "binoculars"
+    static let drinks: String = "wineglass"
+    static let ingredients: String = "circle.grid.2x2"
+    static let bar: String = "table.furniture"
+    static let settings: String = "gear"
+    
+    // Not used
+    static let globe: String = "globe.europe.africa"
+}
