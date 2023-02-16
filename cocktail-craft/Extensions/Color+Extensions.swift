9ad9e7d7c9344f0a8ac927e5df07ff03ee7b0277
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2023-02-16 10:17:09 +0100

    feat(colors): fix contrastColor calculation

--- cocktail-craft/Extensions/Color+Extensions.swift
+++ cocktail-craft/Extensions/Color+Extensions.swift
@@ -8,47 +8,18 @@
 import SwiftUI
 
 extension Color {
-    enum Brightness {
-        case light, medium, dark, transparent
-        
-        private enum Threshold {
-            static let transparent: CGFloat = 0.1
-            static let light: CGFloat = 0.75
-            static let dark: CGFloat = 0.3
-        }
-        
-        init(brightness: CGFloat, alpha: CGFloat) {
-            if alpha < Threshold.transparent {
-                self = .transparent
-            } else if brightness > Threshold.light {
-                self = .light
-            } else if brightness < Threshold.dark {
-                self = .dark
-            } else {
-                self = .medium
-            }
-        }
-    }
-    
-    var brightness: Brightness {
-        var b: CGFloat = 0
-        var a: CGFloat = 0
-        let uiColor = UIColor(self)
-        uiColor.getHue(nil, saturation: nil, brightness: &b, alpha: &a)
-        return .init(brightness: b, alpha: a)
-    }
-    
     var contrastColor: Self {
-        switch brightness {
-        case .light:
-            return .black
-        case .medium:
-            return .black
-        case .dark:
-            return .white
-        case .transparent:
-            return Color.label
-        }
+        var r: CGFloat = 0.0
+        var g: CGFloat = 0.0
+        var b: CGFloat = 0.0
+        var a: CGFloat = 0.0
+        var brightness: CGFloat = 0.0
+        
+        UIColor(self).getRed(&r, green: &g, blue: &b, alpha: &a)
+        
+        // algorithm from: http://www.w3.org/WAI/ER/WD-AERT/#color-contrast
+        brightness = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+        return brightness < 0.6 ? .white : .black
     }
     
 #if os(macOS)
